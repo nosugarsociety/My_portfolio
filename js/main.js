64d47cb2f9500ae184a6c96d80c9276151c116ba
github username <github email address>
2014-12-16 10:21:59 -0500

    change background and change navigation button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
 $(function(){
 
 	// Aside Toggle
-
-	$('#open').click(function(){
+    $('#open').find('i').click(function(){
    		$('aside').toggle({direction:'right'},'slide');
-   		$(this).find('i').toggleClass('great-white');
+   		$('i').toggle();
+        $('#mask').toggleClass('hide');
+// All elements main section move left 400px
+        
+
     });
 
+	//resizing silhouette
+	var $silhouette = $('#silhouette'),
+		$intro = $('#intro');
+        
+
+    function matchHeight() {
+    	$silhouette.height($(window).height());
+        
+
+    	// var silHeight = $silhouette.height(),
+    	// 	introHeight = $intro.height(),
+    	// 	marginTop = (silHeight - introHeight)/2;
+
+    	// //initially push intro below silhouette
+    	// $intro.css({'top': silHeight}); 
+
+    	//set margin above intro to center it.
+    	//$intro.css({'margin-top':marginTop});
+    	// var dataNum = /data-\d+/
+    	// $intro.removeAttr(dataNum);
+
+    	// $intro.attr('data-'+);
+
+    }
+    matchHeight();
+
+    $(window).resize(matchHeight);
+
+
+    //skrollr
+    skrollr.init({
+    	render: function(data){
+    		console.log(data.curTop);
+    	}
+    });
+
+    //Copying Code-background
+    $("#code-background").find("p").clone().clone().insertAfter('#code-background p');
+
+
+
+
 });
+
